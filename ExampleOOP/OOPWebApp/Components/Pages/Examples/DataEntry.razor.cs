af641c7e9151d3a5bdb29d361ae41e3965f9b1d7
Tina Caron <tcaron@nait.ca>
2024-10-16 20:29:52 -0600

    Started Data Entry form for Employment

---
+++ ExampleOOP/OOPWebApp/Components/Pages/Examples/DataEntry.razor.cs
@@ -0,0 +1,17 @@
+﻿using OOPLibrary;
+
+namespace OOPWebApp.Components.Pages.Examples
+{
+
+    public partial class DataEntry
+    {
+        private string feedback = string.Empty;
+
+        //Creating variables to store the form information
+        //Match what is in the class for datatypes
+        private string empTitle = string.Empty;
+        private DateTime empStartDate = DateTime.Today;
+        private SupervisoryLevel empLevel;
+        private double empYears = 0.0;
+    }
+}
