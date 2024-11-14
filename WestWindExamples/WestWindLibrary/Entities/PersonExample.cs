da418a4f55a3ef2f95df2d8b3d54948d70a47ec7
Tina Caron <tcaron@nait.ca>
2024-11-13 21:50:55 -0700

    Finished Data Annotation Examples

--- WestWindExamples/WestWindLibrary/Entities/PersonExample.cs
+++ WestWindExamples/WestWindLibrary/Entities/PersonExample.cs
@@ -37,10 +37,34 @@ namespace WestWindLibrary.Entities
             ErrorMessage = "{0} must be in the format ###-###-###")]
         public string SocialInsuranceNumber { get; set; }
 
+        [Required(ErrorMessage = "You must provide a {0}.")]
+        [EmailAddress]
         public string Email { get; set; }
 
+        //This is for confirmation of the email entered by the user, so much match the Email
+        [Display(Name = "Confirm Email")]
+        [Required(ErrorMessage = "You must provide a {0}.")]
+        [EmailAddress]
+        //Compare the value of one property to another
+        //Takes in the property being compared and can provide an ErrorMessage
+        [Compare(nameof(Email), ErrorMessage = "{1} and {0} must match.")]
         public string ConfirmEmail { get; set; }
 
+        //Business Rule: Birthday cannot be in the future
+        //Use a Custom Validator
+        [CustomValidation(typeof(NotFutureValidation), nameof(NotFutureValidation.Validate))]
         public DateOnly Birthday { get; set; }
+    }
+
+    //Outside of the PersonExample Class
+    public class NotFutureValidation
+    {
+        //provide a static method that can be used to return the validation results.
+        //The Validate Method must have a paramter that is the same datatype
+        //as the Property (Birthdate) that we want to validate
+        public static ValidationResult? Validate(DateOnly date)
+        {
+            return date > DateOnly.FromDateTime(DateTime.Today) ? new ValidationResult("The date cannot be in the future.") : ValidationResult.Success;
+        }
     }
 }
