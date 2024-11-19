bdb006d49f96d5462d902b406895f9aa15a630d4
Tina Caron <tcaron@nait.ca>
2024-11-18 20:18:22 -0700

    Added Product Add

--- WestWindExamples/WestWindLibrary/BLL/ProductServices.cs
+++ WestWindExamples/WestWindLibrary/BLL/ProductServices.cs
@@ -26,5 +26,43 @@ namespace WestWindLibrary.BLL
             return _context.Products.Where(x=>x.CategoryID.Equals(categoryId)).Include(p=>p.Category).Include(p=>p.Supplier).ToList();
         }
         #endregion
+
+        #region CRUD
+        //For an insert we always return an int for identifier as the PK, this is the new PK
+        public int Product_AddProduct(Product product)
+        {
+            //Check if you got data
+            if (product == null)
+            {
+                throw new ArgumentNullException("You must supply the new product information.");
+            }
+
+            //Business Rule Example
+            //Does the Product already exist?
+            bool exists = _context.Products.Any(x => x.SupplierID == product.SupplierID
+                                                && x.ProductName == product.ProductName);
+
+            if (exists)
+            {
+                throw new ArgumentException("Product already exists, cannot add.");
+            }
+
+            //You can check multiple Business rules
+            //You can reuse the exists bool if needed
+
+            //Staging
+            //IMPORTANT - Remember this is only local, meaning it is only local in memory not in the database
+            //This product will not have an ID
+            _context.Products.Add(product);
+
+            //Commit
+            //This sends the data to the database
+            //The info for the changes or inserted records is updated in our DBSet (Products)
+            _context.SaveChanges();
+
+            //Now the new product will have an ID
+            return product.ProductID;
+        }
+        #endregion
     }
 }
--- WestWindExamples/WestWindWeb/Components/Pages/ProductCRUD.razor.cs
+++ WestWindExamples/WestWindWeb/Components/Pages/ProductCRUD.razor.cs
@@ -8,6 +8,7 @@ namespace WestWindWeb.Components.Pages
     {
         private Product product = new();
         private string feedback = string.Empty;
+        private List<string> errorMsgs = [];
         private List<Supplier> suppliers = [];
         private List<Category> categories = [];
 
@@ -15,6 +16,8 @@ namespace WestWindWeb.Components.Pages
         SupplierServices supplierServices { get; set; }
         [Inject]
         CategoryServices categoryServices { get; set; }
+        [Inject]
+        ProductServices productServices { get; set; }
 
         protected override void OnInitialized()
         {
@@ -26,18 +29,50 @@ namespace WestWindWeb.Components.Pages
             }
             catch (Exception ex)
             {
-                feedback = ex.Message;
+                errorMsgs.Add(GetInnerException(ex).Message);
             }
             base.OnInitialized();
         }
         private void OnValidSubmit()
         {
-            feedback = "This be valid!";
-        }
+            //Always clear the messaging first. Always, Always, Always!
+            errorMsgs.Clear();
+            feedback = string.Empty;
 
+            if (product.SupplierID == 0)
+            {
+                //Add to errorMsg
+                //If using EditContext you can add an error to the ValidationSummary for the related field
+                errorMsgs.Add("You must select a supplier.");
+            }
+            if (product.CategoryID == 0)
+            {
+                errorMsgs.Add("You must select a category.");
+            }
+            if (errorMsgs.Count == 0)
+            {
+                try
+                {
+                    int newproductid = productServices.Product_AddProduct(product);
+                    feedback = $"Product {product.ProductName} (ID: {newproductid}) has be added to the database.";
+                }
+                catch (Exception ex)
+                {
+                    errorMsgs.Add(GetInnerException(ex).Message);
+                }
+            }
+
+        }
         private void OnInvalidSubmit()
         {
             feedback = "This be borked!";
+        }
+
+        private Exception GetInnerException(Exception ex)
+        {
+            while (ex.InnerException != null)
+                ex = ex.InnerException;
+            return ex;
         }
     }
 }
